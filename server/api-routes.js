d418180ce0786a374cf4237e83d995ca6b535544
krishavi85 <83726923+krishavi85@users.noreply.github.com>
2026-06-28 14:10:10 -0300

    Register full platform APIs

--- server/api-routes.js
+++ server/api-routes.js
@@ -1,8 +1,10 @@
 function registerApiRoutes(options) {
   const deviceRoutes = require('./device-routes');
   const apkRoutes = require('./apk-routes');
+  const platformRoutes = require('./platform-routes');
   deviceRoutes.registerDeviceRoutes(options.app);
   apkRoutes.registerApkRoutes(options.app, options.upload);
+  platformRoutes.registerPlatformRoutes(options.app);
 }
 
 module.exports.registerApiRoutes = registerApiRoutes;
