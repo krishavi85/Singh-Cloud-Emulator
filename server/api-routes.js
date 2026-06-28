924a6ba6a3d7a1d67b52f4eb86051a87efadc2f6
krishavi85 <83726923+krishavi85@users.noreply.github.com>
2026-06-28 16:44:21 -0300

    Register production persistence, worker, artifact and management APIs

--- server/api-routes.js
+++ server/api-routes.js
@@ -5,13 +5,28 @@ function registerApiRoutes(options) {
   const automationRoutes = require('./automation-routes');
   const shareRoutes = require('./share-routes');
   const workerRoutes = require('./worker-routes');
+  const buildWorkerRoutes = require('./build-worker-routes');
+  const artifactRoutes = require('./artifact-routes');
+  const workspaceArchiveRoutes = require('./workspace-archive-routes');
+  const gitRoutes = require('./git-routes');
+  const managementRoutes = require('./management-routes');
+  const billingRoutes = require('./billing-routes');
+  const systemRoutes = require('./system-routes');
   const equivalenceRoutes = require('./equivalence-routes');
+
   deviceRoutes.registerDeviceRoutes(options.app);
   apkRoutes.registerApkRoutes(options.app, options.upload);
   platformRoutes.registerPlatformRoutes(options.app);
   automationRoutes.registerAutomationRoutes(options.app);
   shareRoutes.registerShareRoutes(options.app);
   workerRoutes.registerWorkerRoutes(options.app);
+  buildWorkerRoutes.registerBuildWorkerRoutes(options.app);
+  artifactRoutes.registerArtifactRoutes(options.app);
+  workspaceArchiveRoutes.registerWorkspaceArchiveRoutes(options.app);
+  gitRoutes.registerGitRoutes(options.app);
+  managementRoutes.registerManagementRoutes(options.app);
+  billingRoutes.registerBillingRoutes(options.app);
+  systemRoutes.registerSystemRoutes(options.app);
   equivalenceRoutes.registerEquivalenceRoutes(options.app);
 }
 
