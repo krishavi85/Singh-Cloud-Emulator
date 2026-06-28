c25390e63245444d9859419b03b16943e6ef2a62
krishavi85 <83726923+krishavi85@users.noreply.github.com>
2026-06-28 14:16:12 -0300

    Register share presentation API

--- server/api-routes.js
+++ server/api-routes.js
@@ -3,10 +3,12 @@ function registerApiRoutes(options) {
   const apkRoutes = require('./apk-routes');
   const platformRoutes = require('./platform-routes');
   const automationRoutes = require('./automation-routes');
+  const shareRoutes = require('./share-routes');
   deviceRoutes.registerDeviceRoutes(options.app);
   apkRoutes.registerApkRoutes(options.app, options.upload);
   platformRoutes.registerPlatformRoutes(options.app);
   automationRoutes.registerAutomationRoutes(options.app);
+  shareRoutes.registerShareRoutes(options.app);
 }
 
 module.exports.registerApiRoutes = registerApiRoutes;
