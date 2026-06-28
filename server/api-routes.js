64343c45155525815d4b9215479d3205e7160839
krishavi85 <83726923+krishavi85@users.noreply.github.com>
2026-06-28 14:18:35 -0300

    Register worker lifecycle APIs

--- server/api-routes.js
+++ server/api-routes.js
@@ -4,11 +4,13 @@ function registerApiRoutes(options) {
   const platformRoutes = require('./platform-routes');
   const automationRoutes = require('./automation-routes');
   const shareRoutes = require('./share-routes');
+  const workerRoutes = require('./worker-routes');
   deviceRoutes.registerDeviceRoutes(options.app);
   apkRoutes.registerApkRoutes(options.app, options.upload);
   platformRoutes.registerPlatformRoutes(options.app);
   automationRoutes.registerAutomationRoutes(options.app);
   shareRoutes.registerShareRoutes(options.app);
+  workerRoutes.registerWorkerRoutes(options.app);
 }
 
 module.exports.registerApiRoutes = registerApiRoutes;
