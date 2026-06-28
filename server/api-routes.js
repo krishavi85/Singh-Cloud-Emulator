0ffe8a2745a93209066e7342b5659ffa1186f140
krishavi85 <83726923+krishavi85@users.noreply.github.com>
2026-06-28 14:11:13 -0300

    Register automation recorder API

--- server/api-routes.js
+++ server/api-routes.js
@@ -2,9 +2,11 @@ function registerApiRoutes(options) {
   const deviceRoutes = require('./device-routes');
   const apkRoutes = require('./apk-routes');
   const platformRoutes = require('./platform-routes');
+  const automationRoutes = require('./automation-routes');
   deviceRoutes.registerDeviceRoutes(options.app);
   apkRoutes.registerApkRoutes(options.app, options.upload);
   platformRoutes.registerPlatformRoutes(options.app);
+  automationRoutes.registerAutomationRoutes(options.app);
 }
 
 module.exports.registerApiRoutes = registerApiRoutes;
